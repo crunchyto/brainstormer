5041b9246b0302da228c706176f5ebd107669fc2
crunchyto <crunchyto@gmail.com>
2025-06-11 18:07:08 +0200

    Modified src/types/auth.ts and src/types/next-auth.d.ts

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -9,6 +9,12 @@ declare module "next-auth" {
   }
 
   interface User {
-    username: string;
+    username?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    username?: string;
   }
 }
